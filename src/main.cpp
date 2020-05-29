e7ad5a3340316b7069b53a9ec3975d0f25c0b927
viktak <github@viktak.com>
2020-05-29 12:38:25 +0300

    wifi connect fix

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,6 @@ RGBPixelData buf[NUMBER_OF_LEDS];
 
 //  Other global variables
 config appConfig;
-char timeout = 30;
 bool isAccessPoint = false;
 bool isAccessPointCreated = false;
 uint8_t oldSecs = -1;
@@ -1207,14 +1206,14 @@ void loop(){
           // Initialize iteration counter
           char attempt = 0;
 
-          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < timeout)) {
+          while ((WiFi.status() != WL_CONNECTED) && (attempt++ < WIFI_CONNECTION_TIMEOUT)) {
             digitalWrite(CONNECTION_STATUS_LED_GPIO, LOW);
             Serial.print(".");
             delay(50);
             digitalWrite(CONNECTION_STATUS_LED_GPIO, HIGH);
             delay(950);
           }
-          if (attempt > WIFI_CONNECTION_TIMEOUT) {
+          if (attempt >= WIFI_CONNECTION_TIMEOUT) {
             Serial.println();
             Serial.println("Could not connect to WiFi");
             delay(100);
