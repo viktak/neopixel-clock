8ca1f4b91a793563ab16606c0e6595d2f05e0498
viktak <github@viktak.com>
2020-11-27 11:58:14 +0200

    -

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "11"
+  #define BUILD_NUMBER "25"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.0.11 - 2020-11-24 18:46:37.751642"
+  #define FIRMWARE_VERSION "v1.0.25 - 2020-11-27 11:57:23.803419"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.0.11"
+  #define FIRMWARE_VERSION_SHORT "v1.0.25"
 #endif
